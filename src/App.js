8696e1e4c8f0cebb5daa414f85382f95409a2ff8
programarivm <info@programarivm.com>
2019-04-14 19:50:23 +0100

    a main navigation bar is added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+      super(props);
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render() {
+      return (
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/"><b>nonet</b></NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/dashboard/">Dashboard</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    IPs
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Source
+                    </DropdownItem>
+                    <DropdownItem>
+                      Destination
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Protocols
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Protocol 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Protocol 2
+                    </DropdownItem>
+                    <DropdownItem>
+                      ...
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
 }
 
 export default App;
