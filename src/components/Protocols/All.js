5536ae19e14aa4dd9809b7d536d6cefcca84ebac
programarivm <info@programarivm.com>
2019-04-22 14:02:11 +0100

    the pie chart is replaced with an horizontal bar in Protocols / All

--- src/components/Protocols/All.js
+++ src/components/Protocols/All.js
@@ -6,27 +6,21 @@ import {
   Row,
   Col
 } from 'reactstrap';
-import {Pie} from 'react-chartjs-2';
+import {HorizontalBar} from 'react-chartjs-2';
 
 const data = {
-  labels: [
-    'Red',
-    'Green',
-    'Yellow'
-  ],
-  datasets: [{
-    data: [300, 50, 100],
-    backgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    '#FFCE56'
-    ],
-    hoverBackgroundColor: [
-    '#FF6384',
-    '#36A2EB',
-    '#FFCE56'
-    ]
-  }]
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [1, 10, 100, 200, 300, 400, 500]
+    }
+  ]
 };
 
 class All extends React.Component {
@@ -40,7 +34,7 @@ class All extends React.Component {
                 <BreadcrumbItem>Protocols</BreadcrumbItem>
                 <BreadcrumbItem active>All</BreadcrumbItem>
               </Breadcrumb>
-              <Pie data={data} />
+              <HorizontalBar data={data} />
             </Col>
           </Row>
         </Container>
