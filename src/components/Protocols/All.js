fce45661a6708d7e42f5b3f81e60e269979746dc
programarivm <info@programarivm.com>
2019-04-23 20:44:52 +0100

    the protocol tree is displayed

--- src/components/Protocols/All.js
+++ src/components/Protocols/All.js
@@ -9,6 +9,17 @@ import {
 import {HorizontalBar} from 'react-chartjs-2';
 
 class All extends React.Component {
+  ProtocolTree(props) {
+    const protocols = props.stats.protocols.tshark;
+    const items = protocols.map((protocol) =>
+      <li>{protocol.name}</li>
+    );
+    return (
+      <ul>{items}</ul>
+    );
+  }
+
+
   render() {
     return (
       <div>
@@ -19,8 +30,21 @@ class All extends React.Component {
                 <BreadcrumbItem>Protocols</BreadcrumbItem>
                 <BreadcrumbItem active>All</BreadcrumbItem>
               </Breadcrumb>
-              <HorizontalBar data={this.props.stats.protocols.chart.bytes} redraw />
-              <HorizontalBar data={this.props.stats.protocols.chart.frames} redraw />
+              <Row>
+                <Col lg="4">
+                  <ul>
+                    {
+                      this.props.stats.protocols.tshark.map(function(item, index) {
+                        return (<li>{'--'.repeat(item.level)} {item.name}</li>)
+                      })
+                    }
+                  </ul>
+                </Col>
+                <Col lg="8">
+                  <HorizontalBar data={this.props.stats.protocols.chart.bytes} redraw />
+                  <HorizontalBar data={this.props.stats.protocols.chart.frames} redraw />
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
