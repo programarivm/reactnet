8f3a39a610df8a9e4445e7e9b3baaa5c490d7848
programarivm <info@programarivm.com>
2019-05-05 00:49:33 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavBar } from "./components/NavBar.js";
-import helpers  from './helpers.js';
+import calc  from './calc.js';
 import './App.css';
 
 const url = 'ws://localhost:3001';
@@ -118,7 +118,7 @@ class App extends Component {
 
     this.ws.onmessage = event => {
       if (event.data !== 'wait') {
-        this.calcStats(JSON.parse(event.data));
+        this.stats(JSON.parse(event.data));
         this.setState({
           n: this.state.n + 1
         });
@@ -134,123 +134,12 @@ class App extends Component {
     };
   }
 
-  calcEndpointsIpv6(endpoints, data) {
-    if (endpoints.history.length === 0) {
-      endpoints.history = data.ips.v6.endpoints;
-    } else {
-      endpoints.labels = [];
-      endpoints.datasets[0].data = [];
-      endpoints.datasets[1].data = [];
-      endpoints.datasets[2].data = [];
-      endpoints.datasets[3].data = [];
-      endpoints.datasets[4].data = [];
-      endpoints.datasets[5].data = [];
-      for (let endpoint of data.ips.v6.endpoints) {
-        let exists = false;
-        for (let history of endpoints.history) {
-          if (history.ip === endpoint.ip) {
-            history.packets = +history.packets + +endpoint.packets;
-            history.bytes = +history.bytes + +endpoint.bytes;
-            history.tx_packets = +history.tx_packets + +endpoint.tx_packets;
-            history.tx_bytes = +history.tx_bytes + +endpoint.tx_bytes;
-            history.rx_packets = +history.rx_packets + +endpoint.rx_packets;
-            history.rx_bytes = +history.rx_bytes + +endpoint.rx_bytes;
-            exists = true;
-            break;
-          }
-        }
-        if (!exists) {
-          endpoints.history.push(endpoint);
-        }
-      }
-      endpoints.history.sort(function (a, b) {
-        return b.bytes - a.bytes;
-      });
-      // the twenty busiest endpoints
-      for (let i = 0; i < 20; i++) {
-        let item = endpoints.history[i];
-        if (item !== undefined) {
-          endpoints.labels.push(item.ip);
-          endpoints.datasets[0].data.push(item.packets);
-          endpoints.datasets[1].data.push(item.bytes);
-          endpoints.datasets[2].data.push(item.tx_packets);
-          endpoints.datasets[3].data.push(item.tx_bytes);
-          endpoints.datasets[4].data.push(item.rx_packets);
-          endpoints.datasets[5].data.push(item.rx_bytes);
-        }
-      }
-    }
-  }
-
-  calcIpv4(occurrences, data) {
-    if (Object.keys(occurrences.history).length === 0) {
-      occurrences.history = data.ips.v4.conv;
-    } else {
-      Object.keys(data.ips.v4.conv).forEach((key) => {
-        occurrences.history.hasOwnProperty(key)
-          ? occurrences.history[key] += data.ips.v4.conv[key]
-          : occurrences.history[key] = data.ips.v4.conv[key];
-      });
-    }
-    let items = helpers.countOccurrences(occurrences.history);
-    occurrences.labels = Object.values(items);
-    occurrences.datasets[0].data = Object.keys(items);
-    occurrences.datasets[0].backgroundColor.push('#'+Math.floor(Math.random()*16777215).toString(16));
-    occurrences.history = helpers.sortObject(occurrences.history);
-  }
-
-  calcIpv6(occurrences, data) {
-    if (Object.keys(occurrences.history).length === 0) {
-      occurrences.history = data.ips.v6.conv;
-    } else {
-      Object.keys(data.ips.v6.conv).forEach((key) => {
-        occurrences.history.hasOwnProperty(key)
-          ? occurrences.history[key] += data.ips.v6.conv[key]
-          : occurrences.history[key] = data.ips.v6.conv[key];
-      });
-    }
-    let items = helpers.countOccurrences(occurrences.history);
-    occurrences.labels = Object.values(items);
-    occurrences.datasets[0].data = Object.keys(items);
-    occurrences.datasets[0].backgroundColor.push('#'+Math.floor(Math.random()*16777215).toString(16));
-    occurrences.history = helpers.sortObject(occurrences.history);
-  }
-
-  calcProtocols(protocols, data) {
-    protocols.chart.bytes.labels = [];
-    protocols.chart.frames.labels = [];
-    protocols.chart.bytes.datasets[0].data = [];
-    protocols.chart.frames.datasets[0].data = [];
-    for (let item of data.protocols) {
-      let exists = false;
-      for (let protocol of protocols.tshark) {
-        if (item.name === protocol.name && item.level === protocol.level && item.parent_name === protocol.parent_name) {
-          protocol.bytes = parseInt(protocol.bytes) + parseInt(item.bytes);
-          protocol.frames = parseInt(protocol.frames) + parseInt(item.frames);
-          protocols.chart.bytes.labels.push(protocol.name);
-          protocols.chart.frames.labels.push(protocol.name);
-          protocols.chart.bytes.datasets[0].data.push(protocol.bytes);
-          protocols.chart.frames.datasets[0].data.push(protocol.frames);
-          exists = true;
-          break;
-        }
-      }
-      if (!exists) {
-        protocols.tshark.push(item);
-        protocols.chart.bytes.labels.push(item.name);
-        protocols.chart.frames.labels.push(item.name);
-        protocols.chart.bytes.datasets[0].data.push(item.bytes);
-        protocols.chart.frames.datasets[0].data.push(item.frames);
-      }
-    }
-  }
-
-  calcStats(data) {
+  stats(data) {
     let newState = Object.assign({}, this.state);
-    this.calcEndpointsIpv6(newState.ips.v6.chart.endpoints, data);
-    this.calcIpv4(newState.ips.v4.chart.occurrences, data);
-    this.calcIpv6(newState.ips.v6.chart.occurrences, data);
-    this.calcProtocols(newState.protocols, data);
+    calc.ips.v4.chart.occurrences(newState.ips.v4.chart.occurrences, data);
+    calc.ips.v6.chart.occurrences(newState.ips.v6.chart.occurrences, data);
+    calc.ips.v6.chart.endpoints(newState.ips.v6.chart.endpoints, data);
+    calc.protocols(newState.protocols, data);
     this.setState(newState);
   }
 
---
+++ src/calc.js
@@ -0,0 +1,123 @@
+import helpers  from './helpers.js';
+
+const calc = {
+  ips: {
+    v4: {
+      chart: {
+        occurrences: function(obj, data) {
+          if (Object.keys(obj.history).length === 0) {
+            obj.history = data.ips.v4.conv;
+          } else {
+            Object.keys(data.ips.v4.conv).forEach((key) => {
+              obj.history.hasOwnProperty(key)
+                ? obj.history[key] += data.ips.v4.conv[key]
+                : obj.history[key] = data.ips.v4.conv[key];
+            });
+          }
+          let items = helpers.countOccurrences(obj.history);
+          obj.labels = Object.values(items);
+          obj.datasets[0].data = Object.keys(items);
+          obj.datasets[0].backgroundColor.push('#'+Math.floor(Math.random()*16777215).toString(16));
+          obj.history = helpers.sortObject(obj.history);
+          }
+        }
+      },
+    v6: {
+      chart: {
+        occurrences: function(obj, data) {
+          if (Object.keys(obj.history).length === 0) {
+            obj.history = data.ips.v6.conv;
+          } else {
+            Object.keys(data.ips.v6.conv).forEach((key) => {
+              obj.history.hasOwnProperty(key)
+                ? obj.history[key] += data.ips.v6.conv[key]
+                : obj.history[key] = data.ips.v6.conv[key];
+            });
+          }
+          let items = helpers.countOccurrences(obj.history);
+          obj.labels = Object.values(items);
+          obj.datasets[0].data = Object.keys(items);
+          obj.datasets[0].backgroundColor.push('#'+Math.floor(Math.random()*16777215).toString(16));
+          obj.history = helpers.sortObject(obj.history);
+        },
+        endpoints: function(obj, data) {
+          if (obj.history.length === 0) {
+            obj.history = data.ips.v6.endpoints;
+          } else {
+            obj.labels = [];
+            obj.datasets[0].data = [];
+            obj.datasets[1].data = [];
+            obj.datasets[2].data = [];
+            obj.datasets[3].data = [];
+            obj.datasets[4].data = [];
+            obj.datasets[5].data = [];
+            for (let endpoint of data.ips.v6.endpoints) {
+              let exists = false;
+              for (let history of obj.history) {
+                if (history.ip === endpoint.ip) {
+                  history.packets = +history.packets + +endpoint.packets;
+                  history.bytes = +history.bytes + +endpoint.bytes;
+                  history.tx_packets = +history.tx_packets + +endpoint.tx_packets;
+                  history.tx_bytes = +history.tx_bytes + +endpoint.tx_bytes;
+                  history.rx_packets = +history.rx_packets + +endpoint.rx_packets;
+                  history.rx_bytes = +history.rx_bytes + +endpoint.rx_bytes;
+                  exists = true;
+                  break;
+                }
+              }
+              if (!exists) {
+                obj.history.push(endpoint);
+              }
+            }
+            obj.history.sort(function (a, b) {
+              return b.bytes - a.bytes;
+            });
+            // the twenty busiest endpoints
+            for (let i = 0; i < 20; i++) {
+              let item = obj.history[i];
+              if (item !== undefined) {
+                obj.labels.push(item.ip);
+                obj.datasets[0].data.push(item.packets);
+                obj.datasets[1].data.push(item.bytes);
+                obj.datasets[2].data.push(item.tx_packets);
+                obj.datasets[3].data.push(item.tx_bytes);
+                obj.datasets[4].data.push(item.rx_packets);
+                obj.datasets[5].data.push(item.rx_bytes);
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  protocols: function(obj, data) {
+    obj.chart.bytes.labels = [];
+    obj.chart.frames.labels = [];
+    obj.chart.bytes.datasets[0].data = [];
+    obj.chart.frames.datasets[0].data = [];
+    for (let item of data.protocols) {
+      let exists = false;
+      for (let protocol of obj.tshark) {
+        if (item.name === protocol.name && item.level === protocol.level && item.parent_name === protocol.parent_name) {
+          protocol.bytes = +protocol.bytes + +item.bytes;
+          protocol.frames = +protocol.frames + +item.frames;
+          obj.chart.bytes.labels.push(protocol.name);
+          obj.chart.frames.labels.push(protocol.name);
+          obj.chart.bytes.datasets[0].data.push(protocol.bytes);
+          obj.chart.frames.datasets[0].data.push(protocol.frames);
+          exists = true;
+          break;
+        }
+      }
+      if (!exists) {
+        obj.tshark.push(item);
+        obj.chart.bytes.labels.push(item.name);
+        obj.chart.frames.labels.push(item.name);
+        obj.chart.bytes.datasets[0].data.push(item.bytes);
+        obj.chart.frames.datasets[0].data.push(item.frames);
+      }
+    }
+  }
+}
+
+export default calc;
